67edc717071904c93c6821273bdf36c124590e5d
Besoga1001 <bernardo@dblink.com.br>
2022-06-01 19:30:59 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+tela1 = 1
+
+print('Bem-vindo ao aplicativo de agendamento da Barbearia X')
+
+while (tela1 == 1):
+    tipoUsuario = int(input('Você é cliente ou empresa? \nDigite 1 para cliente e 2 para empresa: '))
+
+    if (tipoUsuario == 1):
+        tela1 = 0
+        import login_cliente
+
+    elif(tipoUsuario == 2):
+        tela1 = 0
+        import login_empresa
+    else:
+        print('Opção Inválida, favor digitar novamente.')
+        tela1 = 1
