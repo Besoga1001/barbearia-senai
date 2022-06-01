779f4fba70acd526aaac7c883da4bdb645feee50
Besoga1001 <bernardo@dblink.com.br>
2022-06-01 20:52:45 -0300

    Initial Commit

---
+++ cadastro_client.py
@@ -0,0 +1,4 @@
+print('Cadastro Cliente\nEntre com suas informações: ')
+user_name_cadastro = input(str('Username: '))
+senha_cadastro = input(str('Senha: '))
+confirmar_senha_cadastro = input(str('Confirmar Senha: '))
